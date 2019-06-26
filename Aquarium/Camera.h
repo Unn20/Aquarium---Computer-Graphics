2702517f78f90010d6b456ab62150a9bf0019cd4
Maciej Leszczyk <maciox487@gmail.com>
2019-06-26 11:47:25 +0200

    camera zoom fix

--- Aquarium/Camera.h
+++ Aquarium/Camera.h
@@ -13,7 +13,7 @@ const float PITCH = 0.0f;
 const float SPEED = 2.5f;
 const float SENSITIVITY = 0.1f;
 const float ZOOM = 45.0f;
-const float RADIUS = 6.0f;
+const float RADIUS = 8.0f;
 
 // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
 enum Camera_Movement {
